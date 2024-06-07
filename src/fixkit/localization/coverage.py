8d1f5ceb0a01d5c65f5fd1a03beec1703d3d85e4
Marius Smytzek <marius.smytzek@cispa.de>
2024-06-07 12:41:41 +0200

    renamed to fixkit

---
+++ src/fixkit/localization/coverage.py
@@ -0,0 +1,121 @@
+"""
+The coverage module provides the necessary tools to localize a fault using coverage.py information.
+"""
+
+import json
+import os
+import re
+import shutil
+import subprocess
+from typing import Optional, List, Dict
+
+from sflkit.analysis.spectra import Spectrum
+
+from fixkit.localization.localization import Localization
+from fixkit.localization.location import WeightedLocation
+
+
+# Regular expression for parsing the context of a coverage line.
+CONTEXT_PATTERN = re.compile(r"(?P<test>[^|]+)\|run")
+
+
+class CoverageLocalization(Localization):
+    """
+    Class for localizing a fault using coverage.py information.
+    """
+
+    def __init__(
+        self,
+        src: os.PathLike,
+        cov: str,
+        failing: Optional[List[str]] = None,
+        passing: Optional[List[str]] = None,
+        tests: Optional[List[str]] = None,
+        out: Optional[os.PathLike] = None,
+        env: Optional[Dict[str, str]] = None,
+        metric: Optional[str] = None,
+    ):
+        """
+        Initialize the coverage localization.
+        :param os.PathLike src: The source directory of the project.
+        :param str cov: The coverage.py data prefix.
+        :param Optional[List[str]] failing: The set of failing tests, will override tests.
+        :param Optional[List[str]] passing: The set of passing tests, will override tests.
+        :param Optional[List[str]] tests: The set of all tests, as an alternative when an oracle exists.
+        :param Optional[os.PathLike] out: The output directory for the localization.
+        :param Optional[Dict[str, str]] env: The environment to run the tests in.
+        :param Optional[str] metric: The metric to use for the localization.
+        """
+        super().__init__(src, failing, passing, tests, out, env, metric)
+        self.cov = cov
+        self.spectra = list()
+
+    def run_preparation(self):
+        """
+        Run the preparation for the localization by executing the tests and collecting the coverage data.
+        """
+        shutil.copytree(self.src, self.out, dirs_exist_ok=True)
+        # Get the passing and failing tests.
+        passing_failing = (self.passing or set()) | (self.failing or set())
+        tests = list(passing_failing or self.tests or {"tests"})
+        # Run the tests and collect the coverage data.
+        subprocess.run(
+            [
+                "python",
+                "-m",
+                "pytest",
+                f"--rootdir={self.out}",
+                "--cov",
+                self.cov,
+                "--cov-context",
+                "test",
+                "--json-report",
+            ]
+            + tests,
+            cwd=self.out,
+            env=self.env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        # Parse the coverage data into a list of passing and failing spectra.
+        self.passing = set()
+        self.failing = set()
+        with open(self.out / ".report.json") as fp:
+            results = json.load(fp)
+        for result in results["tests"]:
+            if result["outcome"] == "passed":
+                self.passing.add(result["nodeid"])
+            else:
+                self.failing.add(result["nodeid"])
+        subprocess.run(
+            ["coverage", "json", "-o", "tmp.json", "--show-context"],
+            cwd=self.out,
+            env=self.env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        with open(self.out / "tmp.json") as fp:
+            coverage_data = json.load(fp)
+        for file in coverage_data["files"]:
+            for line in coverage_data["files"][file]["executed_lines"]:
+                po, fo = 0, 0
+                for test in coverage_data["files"][file]["contexts"][str(line)]:
+                    match = CONTEXT_PATTERN.match(test)
+                    if match:
+                        test = match.group("test")
+                        if test in self.passing:
+                            po += 1
+                        elif test in self.failing:
+                            fo += 1
+                pn, fn = len(self.passing) - po, len(self.failing) - fo
+                self.spectra.append(Spectrum(file, line, po, pn, fo, fn))
+
+    def get_suggestions(self) -> List[WeightedLocation]:
+        """
+        Get the suggestions of the localization by calculating the metric based on the spectra.
+        :return List[WeightedLocation]: The list of weighted locations.
+        """
+        return [WeightedLocation(s.file, s.line, self.metric(s)) for s in self.spectra]
+
+
+__all__ = ["CoverageLocalization"]
---
+++ src/fixkit/repair/pykali.py
@@ -0,0 +1,154 @@
+"""
+The pykali module provides the necessary tools to repair a fault using kali.
+pykali is not really used for repairs. It is used to find under-specified bugs and weak test cases.
+"""
+
+import os
+import random
+from typing import List, Optional
+
+from fixkit.candidate import Candidate, GeneticCandidate
+from fixkit.fitness.metric import GenProgFitness
+from fixkit.genetic.crossover import OnePointCrossover
+from fixkit.genetic.minimize import DDMutationMinimizer
+from fixkit.genetic.operators import (Delete, ModifyIfToFalse, ModifyIfToTrue, 
+                                     InsertReturn0, InsertReturnNone, InsertReturnString, 
+                                     InsertReturnTuple, InsertReturnList)
+from pyrep.localization import Localization
+from fixkit.localization.location import WeightedLocation
+from fixkit.repair.repair import GeneticRepair
+from fixkit.genetic.selection import UniversalSelection, Selection
+from fixkit.search.search import SearchStrategy, ExhaustiveStrategy
+
+
+class PyKali(GeneticRepair):
+    """
+    Class for repairing a fault using Kali.
+    """
+
+    def __init__(
+        self,
+        initial_candidate: Candidate,
+        localization: Localization,
+        max_generations: int,
+        w_mut: float,
+        selection: Selection = None,
+        workers: int = 1,
+        out: os.PathLike = None,
+        w_pos_t: float = 1,
+        w_neg_t: float = 10,
+        is_t4p: bool = False,
+        line_mode: bool = False,
+    ):
+        """
+        Initialize the Kali repair.
+        :param GeneticCandidate initial_candidate: The initial candidate to start the repair.
+        :param Localization localization: The localization to use for the repair.
+        :param int population_size: The size of the population.
+        :param int max_generations: The maximum number of generations.
+        :param float w_mut: The mutation rate, i.e., the probability of a mutation.
+        :param Selection selection: The selection operator to use for the repair.
+        :param int workers: The number of workers to use for the evaluation of the fitness.
+        :param os.PathLike out: The working directory for the repair.
+        :param float w_pos_t: The weight for the positive test cases.
+        :param float w_neg_t: The weight for the negative test cases.
+        """
+        self.metric = GenProgFitness(set(), set(), w_pos_t=w_pos_t, w_neg_t=w_neg_t) #still uses GenProgFitness -> ??
+        super().__init__(
+            initial_candidate=initial_candidate,
+            fitness=self.metric, 
+            localization=localization,
+            population_size=1,
+            max_generations=max_generations, 
+            w_mut=w_mut,
+            operators=[Delete, ModifyIfToTrue, ModifyIfToFalse, 
+                       InsertReturnList, InsertReturnTuple, InsertReturn0, 
+                       InsertReturnString, InsertReturnNone], 
+            selection=selection or UniversalSelection(),
+            crossover_operator=OnePointCrossover(),
+            minimizer=DDMutationMinimizer(),
+            workers=workers,
+            out=out,
+            is_t4p=is_t4p,
+            line_mode=line_mode,
+        )
+
+    @classmethod
+    def from_source(
+        cls, src: os.PathLike, excludes: Optional[List[str]] = None, *args, **kwargs
+    ) -> "GeneticRepair": 
+        """
+        Abstract method for creating a genetic repair from the source.
+        :param os.PathLike src: The source directory of the project.
+        :param Optional[List[str]] excludes: The set of files to exclude from the statement search.
+        :param args: A list of arguments.
+        :param kwargs: A dictionary of keyword arguments.
+        :return GeneticRepair: The genetic repair created from the source.
+        """
+        return cls._from_source(src, excludes, *args, **kwargs)
+
+    @staticmethod
+    def _from_source(
+        src: os.PathLike,
+        excludes: Optional[List[str]],
+        localization: Localization,
+        max_generations: int,
+        w_mut: float,
+        selection: Selection = None,
+        workers: int = 1,
+        out: os.PathLike = None,
+        w_pos_t: float = 1,
+        w_neg_t: float = 10,
+        is_t4p: bool = False,
+        line_mode: bool = False,
+    ) -> "PyKali": 
+        """
+        Create a Kali repair from the source.
+        :param os.PathLike src: The source directory of the project.
+        :param Optional[List[str]] excludes: The set of files to exclude from the statement search.
+        :param Localization localization: The localization to use for the repair.
+        :param int population_size: The size of the population.
+        :param int max_generations: The maximum number of generations.
+        :param float w_mut: The mutation rate, i.e., the probability of a mutation.
+        :param Selection selection: The selection operator to use for the repair.
+        :param int workers: The number of workers to use for the evaluation of the fitness.
+        :param os.PathLike out: The working directory for the repair.
+        :param float w_pos_t: The weight for the positive test cases.
+        :param float w_neg_t: The weight for the negative test cases.
+        :return PyGenProg: The GenProg repair created from the source.
+        """
+        return PyKali(
+            initial_candidate=PyKali.get_initial_candidate(src, excludes, line_mode),
+            localization=localization,
+            max_generations=max_generations,
+            w_mut=w_mut,
+            selection=selection,
+            workers=workers,
+            out=out,
+            w_pos_t=w_pos_t,
+            w_neg_t=w_neg_t,
+            is_t4p=is_t4p,
+            line_mode=line_mode,
+        )
+
+    def localize(self) -> List[WeightedLocation]:
+        """
+        Localize the fault using the localization and report the passing and failing tests to the metric.
+        :return List[WeightedLocation]: The list of weighted locations.
+        """
+        suggestions = super().localize()
+        self.metric.passing, self.metric.failing = (
+            self.localization.passing,
+            self.localization.failing,
+        )
+        return suggestions
+    
+    def get_search_strategy(self) -> SearchStrategy:
+        return ExhaustiveStrategy(
+            operators=self.operator,
+            suggestions=self.localize() # or self.suggestions
+        )
+
+
+
+__all__ = ["PyKali"]
